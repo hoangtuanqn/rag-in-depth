8718a1ad17abf4493477e448939ce8a7cb1c9982
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-04-26 18:36:07 +0700

    feat: add slipt-document feature

---
+++ features/slipt-document.ts
@@ -0,0 +1,29 @@
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters"
+const MARKDOWN_SEPARATORS = [
+  "\n#{1,6} ",
+  "```\n",
+  "\n\\*\\*\\*+\\n",
+  "\n---+\n",
+  "\n___+\n",
+  "\n\n",
+  "\n",
+  " ",
+  ""
+]
+export const splitDocument = async (docs: any[]) => {
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1200,      // số ký tự mỗi chunk
+        chunkOverlap: 200,    // overlap giữa các chunk để không mất context
+        separators: MARKDOWN_SEPARATORS,
+    });
+    const chunks = await splitter.splitDocuments(docs);
+
+    //  console.log(`Split into ${chunks.length} chunks`);
+       for (const chunk of chunks) {
+        console.log("----------------------")
+        console.log("Content:", chunk.pageContent);
+        // console.log("Metadata:", chunk.metadata);
+    }
+
+    return chunks;
+}
