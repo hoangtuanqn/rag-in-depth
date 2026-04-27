3b3b26df900f587859fc2eedcacecd465208884f
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-04-27 23:27:05 +0700

    feat: format source

--- features/load-all-pdtf.ts
+++ features/load-all-pdtf.ts
@@ -1,36 +1,33 @@
-import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-import { TextLoader } from "@langchain/classic/document_loaders/fs/text"
-import fg from "fast-glob";
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { TextLoader } from '@langchain/classic/document_loaders/fs/text';
+import fg from 'fast-glob';
 
 export const loadAllPDFs = async (folderPath: string) => {
-    const paths = await fg(`${folderPath}/**/*.{pdf,txt}`);
-    console.log("Found files:", paths.length);
+  const paths = await fg(`${folderPath}/**/*.{pdf,txt}`);
+  console.log('Found files:', paths.length);
 
-    const allDocPdfs = [];
-    for(const path of paths) {
-        let loader;
-        if(path.endsWith(".pdf")) {
-            loader = new PDFLoader(path);
-        } else if(path.endsWith(".txt")) {
-            loader = new TextLoader(path);
-        } else {
-            continue;
-        }
-
-
-        const docs = await loader.load();
-
-        const enchired = docs.map(doc => ({
-            ...doc,
-            metadata: {
-                ...doc.metadata,
-                source: path
-            }
-        }));
-        // console.log(enchired);
-        allDocPdfs.push(...enchired);
+  const allDocPdfs = [];
+  for (const path of paths) {
+    let loader;
+    if (path.endsWith('.pdf')) {
+      loader = new PDFLoader(path);
+    } else if (path.endsWith('.txt')) {
+      loader = new TextLoader(path);
+    } else {
+      continue;
     }
 
-    return allDocPdfs;
+    const docs = await loader.load();
+    const enchired = docs.map((doc) => ({
+      ...doc,
+      metadata: {
+        ...doc.metadata,
+        source: path,
+      },
+    }));
+    // console.log(enchired);
+    allDocPdfs.push(...enchired);
+  }
 
-}
+  return allDocPdfs;
+};
--- rag.ts
+++ rag.ts
@@ -1,11 +1,9 @@
-import { loadAllPDFs } from "./features/load-all-pdtf";
-import { splitDocument } from "./features/slipt-document";
-
-
+import { loadAllPDFs } from './features/load-all-pdtf';
+import { splitDocument } from './features/slipt-document';
 
 const main = async () => {
-    const docs = await loadAllPDFs('./papers');
-    const chunks = await splitDocument(docs);
-}
+  const docs = await loadAllPDFs('./papers');
+  const chunks = await splitDocument(docs);
+};
 
 main();
